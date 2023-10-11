53f97c3d50d4902c0314723ba88bcb3acb4876e0
Jared <jared.nishikawa@gmail.com>
2023-10-11 12:55:01 -0600

    revamp index.html, add ssl/local toggle

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, redirect, request
 import uuid
+import os
 
 app = Flask(__name__)
+
+def sanitize(s):
+    ok = ""
+    for c in s:
+        if (c >= '0' and c <= '9') or (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z'):
+
+            ok += c
+    return ok
 
 @app.get("/<path>.js")
 def any_js(path):
@@ -17,4 +26,21 @@ def index():
 def board(path):
     return render_template("board.html")
 
-app.run(port=8000)
+@app.post("/new")
+def new_board():
+    print(request.form)
+    board_id = request.form.get("board_id")
+    board_id = sanitize(board_id)
+
+    if not board_id.strip():
+        board_id = uuid.uuid4().hex
+    return redirect(f"/{board_id}")
+
+local = os.getenv("BOARD_LOCAL")
+cert_dir = os.getenv("BOARD_CERT_DIR")
+if local == "false":
+    context = (f"{cert_dir}/fullchain.pem", f"{cert_dir}/privkey.pem")
+    app.run(host="0.0.0.0", port=8080, ssl_context=context)
+else:
+    app.run(port=8080)
+
